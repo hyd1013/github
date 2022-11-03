753dabd5d2d7f0d25b8e77bb5b245df6280f3eb5
hyd1013 <90883233+hyd1013@users.noreply.github.com>
2022-11-03 09:56:34 +0800

    GitHub访问接口第一次修改

---
+++ src/main/java/cn/bdqn/controller/GitHubController.java
@@ -0,0 +1,13 @@
+package cn.bdqn.controller;
+
+/**
+ * @ClassName: GitHubController
+ * @Description:
+ * @Date: 2022-11-03 09:48:47
+ * @Author: HuangYaoDong
+ */
+public class GitHubController {
+
+
+
+}
